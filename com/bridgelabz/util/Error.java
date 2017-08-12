5aabcd996c3aa87ba7ebea322a38661e93d5cc13
Tauqueeransari <30769996+Tauqueeransari@users.noreply.github.com>
2017-08-12 23:50:31 +0530

    Add files via upload

---
+++ com/bridgelabz/util/Error.java
@@ -0,0 +1,15 @@
+/***************************************************************************
+* Purpose : run time error java.lang.NoSuchMethodError
+*
+* @author : Tauqueer ansari
+* @version : 1.0
+* @since : 10-08-2017
+****************************************************************************/
+
+
+public class Error {
+   public String getdisplay() {
+     return "display";
+}
+}
+ 
